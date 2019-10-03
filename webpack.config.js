5e7f6c4648f0c5466e4bf3db710dac0da0391c49
midsever <david@midsever.com>
2019-10-03 14:21:11 -0400

    Added CDN loading to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,55 @@
 const path = require("path");
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js"
+  },
   devtool: "inline-source-map",
-  resolve: {
-    extensions: [".js"]
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "./")
-  },
   devServer: {
-    contentBase: path.join(__dirname, "./dist/"),
-    compress: true,
-    port: 9000,
-    inline: true,
+    contentBase: "./dist",
+    hot: true,
+    port: 8000,
     open: true
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
   plugins: [
-    new CopyPlugin([
-      { from: 'src', to: 'dist' }
-    ]),
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement"
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: 'jquery',
+          entry: {
+            path: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js',
+            type: 'js'
+          },
+          global: 'jQuery',
+        },
+        {
+          module: 'google-roboto',
+          entry: {
+            path: 'https://fonts.googleapis.com/css?family=Roboto',
+            type: 'css',
+          },
+        }
+      ]
+    })
   ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
